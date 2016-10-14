7c59287cddbbbb358b6b787346cf0d4bae7e7717
Hongjie Xin <hongjie.xin@sas.com>
2016-10-14 10:17:55 -0400

    Disable CSRF

--- src/main/java/hello/config/WebSecurityConfig.java
+++ src/main/java/hello/config/WebSecurityConfig.java
@@ -13,6 +13,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+          .csrf().disable()     // Disable csrf
+          						// CSRF (Cross-Site Request Forgery) protection
+          						// default is on. However if you plan to use it, you need ensure that 
+          						// you include the CSRF token in all PATCH, POST, PUT, and DELETE methods.
+          						// For my case, I am lazy to find it how the swagger-ui will work with csrf 
             .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
